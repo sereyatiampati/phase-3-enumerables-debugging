a2060b5d41155e9859e065fc9157e9813269affd
Sereya Tiampati <sereyatiampatistudies@gmail.com>
2023-02-09 02:23:05 +0300

    debugging with pry while enumerating

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,1 +1,20 @@
 # Write your code here
+require 'pry'
+
+def reverse_string
+    str= "Hello there, and how are you?"
+    new_string= str.split.map{ |variable|  variable.reverse}.join(' ')
+end
+
+#puts reverse_string
+def reverse_each_word(sentence)
+    words = sentence.split
+    reversed_words = []
+
+    words.each do |word|
+        reversed_words<< word.reverse
+    end
+    reversed_words.join(' ')
+  end
+
+  puts reverse_each_word("Hello there, and how are you?")
